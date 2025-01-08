c52081fbcb17d281eb7a984b720aeb1a462b2b3c
Mookie WAF <balericaclass6@gmail.com>
2025-01-07 22:49:12 -0600

    Update main.tf
    test7

--- main.tf
+++ main.tf
@@ -48,14 +48,14 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-0e9085e60087ce171"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
+              yum update
+              yum install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
